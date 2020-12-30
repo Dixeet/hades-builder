f05b7f6ccf0f04b545b22badb64391276190e4c3
Robin Lebert <robin.lebert@gmail.com>
2020-12-30 11:05:57 +0100

    chore: proxy to hades wiki

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,4 +45,11 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  serverMiddleware: [
+    {
+      path: '/hades',
+      handler: '~/server/cors.js',
+    },
+  ],
 };
---
+++ server/cors.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+export default function (req, res) {
+  axios.defaults.baseURL = 'https://hades.gamepedia.com';
+  axios.defaults.headers.common.Referer = 'https://hades.gamepedia.com/';
+  axios.defaults.headers.common.Origin = 'https://hades.gamepedia.com/';
+  axios
+    .get(req.url)
+    .then((response) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(response.data));
+    })
+    .catch(() => {
+      res.statusCode = 500;
+      res.statusMessage = 'Failed to request hades';
+      res.end();
+    });
+}
