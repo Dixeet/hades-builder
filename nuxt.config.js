e431394ecf56329203c65917f3c12143703cf603
Robin Lebert <robin.lebert@gmail.com>
2020-12-30 08:15:33 +0100

    style: add basic darkly css theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,13 +14,15 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [{ src: '~assets/css/main.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
+
+  buefy: { css: false },
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
@@ -43,4 +45,4 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-}
+};
