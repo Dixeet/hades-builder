08d8870b32d7588cf95e82847e1728cf214be7a9
Robin Lebert <robin.lebert@gmail.com>
2021-01-03 18:49:22 +0100

    feat: Add TalentPicker

---
+++ components/TalentsPicker.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <b-field v-for="(talent, index) in talents" :key="talent.id">
+      <b-radio-button v-model="talentsPicked[index].type" native-value="red">
+        <div class="columns is-mobile is-vcentered">
+          <div class="column is-3"><div v-html="talent.red.ability"></div></div>
+          <div class="column is-hidden-mobile"><div v-html="talent.red.description"></div></div>
+          <div class="column is-hidden-mobile is-3"><div v-html="talent.red.ranks"></div></div>
+        </div>
+      </b-radio-button>
+      <b-radio-button v-model="talentsPicked[index].type" native-value="green">
+        <div class="columns is-mobile is-vcentered">
+          <div class="column is-3"><div v-html="talent.green.ability"></div></div>
+          <div class="column is-hidden-mobile"><div v-html="talent.green.description"></div></div>
+          <div class="column is-hidden-mobile is-3"><div v-html="talent.green.ranks"></div></div>
+        </div>
+      </b-radio-button>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    talents: {
+      type: Array,
+      required: true,
+    },
+    talentsPicked: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+input {
+  display: none;
+}
+/deep/ {
+  input {
+    display: none;
+  }
+  .control {
+    flex: none;
+    width: 50%;
+  }
+  .b-radio.radio.button {
+    display: block;
+    white-space: normal;
+    height: 100%;
+    .columns {
+      margin-top: 0;
+      margin-bottom: 0;
+      height: 100%;
+    }
+  }
+  img {
+    vertical-align: middle;
+  }
+  .image {
+    display: inline;
+  }
+  .image img {
+    display: inline;
+    width: auto;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,70 @@
 <template>
-  <div>Home</div>
+  <section class="section">
+    <b-collapse class="card" animation="slide" :open="false">
+      <div slot="trigger" slot-scope="props" class="card-header" role="button">
+        <p class="card-header-title">Mirror of Night Talents</p>
+        <a class="card-header-icon">
+          <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"> </b-icon>
+        </a>
+      </div>
+      <div class="card-content">
+        <TalentsPicker :talents="talents" :talents-picked="build.talents" />
+      </div>
+    </b-collapse>
+  </section>
 </template>
 
 <script>
+import TalentsPicker from '../components/TalentsPicker';
+
 export default {
   name: 'HomePage',
-  asyncData({ $data }) {
-    return {};
+  components: {
+    TalentsPicker,
+  },
+  asyncData({ $data, query }) {
+    const build = {
+      talents: parseQueryTalents($data.talents, query),
+    };
+    return {
+      ...$data,
+      build,
+    };
+  },
+  watch: {
+    'build.talents': {
+      handler() {
+        const talents = [];
+        this.build.talents.forEach((talent) => {
+          if (talent.type) {
+            talents.push([talent.id, talent.type === 'red' ? 0 : 1]);
+          }
+        });
+        this.$router.push({ query: { talents: JSON.stringify(talents) } });
+      },
+      deep: true,
+    },
   },
 };
+
+function parseQueryTalents(talents, query) {
+  const buildTalents = talents.map((talent) => ({
+    id: talent.id,
+    type: '',
+  }));
+  if (query.talents) {
+    const queryTalents = JSON.parse(query.talents);
+    queryTalents.forEach((queryTalent) => {
+      const id = queryTalent[0];
+      const type = queryTalent[1] === 0 ? 'red' : 'green';
+      talents.some((talent, index) => {
+        if (talent.id === id) {
+          buildTalents[index].type = type;
+        }
+        return talent.id === id;
+      });
+    });
+  }
+  return buildTalents;
+}
 </script>
