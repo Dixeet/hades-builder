f3cf8e1df74d93c11511c1ebddb9c21e6bbac24d
Robin Lebert <robin.lebert@gmail.com>
2021-01-09 12:57:38 +0100

    feat: Add parameter to Card component

--- components/Card.vue
+++ components/Card.vue
@@ -2,9 +2,11 @@
   <b-collapse class="card" animation="slide" :open="open">
     <template #trigger="{ open }">
       <div class="card-header" role="button">
-        <p class="card-header-title">{{ title }}</p>
-        <a class="card-header-icon">
-          <b-icon :icon="open ? 'menu-down' : 'menu-up'"> </b-icon>
+        <img v-if="image" class="ml-2" :src="image" :alt="title" />
+        <p class="card-header-title" :class="{ 'pl-1': image }">{{ title }}</p>
+        <a class="card-header-icon" @click="close ? $emit('delete') : ''">
+          <b-icon v-if="close" icon="close"> </b-icon>
+          <b-icon v-else :icon="open ? 'menu-down' : 'menu-up'"> </b-icon>
         </a>
       </div>
     </template>
@@ -25,8 +27,20 @@ export default {
       type: Boolean,
       default: () => true,
     },
+    image: {
+      type: String,
+      default: () => '',
+    },
+    close: {
+      type: Boolean,
+      default: () => false,
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+img {
+  max-height: 40px;
+}
+</style>
